4191e7b95cba3b383a4f7548f4d57146e4881b44
naverca <veritasqua@naver.com>
2021-08-26 09:48:58 +0900

    다시 한번 로컬리파지토리에 커밋~!!!

--- src/hellogit/HelloWorld.java
+++ src/hellogit/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("지금 git 수업 중");
+		System.out.println("집중하세요~~ 단, 한번만 수업합니다.");
 	}
 }
