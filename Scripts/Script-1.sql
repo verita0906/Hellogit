e7f54f9fd3f485e37627cd49ec61a41e03069315
naverca <veritasqua@naver.com>
2021-08-26 15:51:10 +0900

    깃허브 작업의 시작 환영해~!

---
+++ Scripts/Script-1.sql
@@ -0,0 +1,2 @@
+select code, name
+	from title;
--- src/hellogit/HelloWorld.java
+++ src/hellogit/HelloWorld.java
@@ -4,15 +4,5 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("지금 Git 수업 중");
-		System.out.println("집중 하세요.~~~ 단 한번만 수업합니다.");
-		test();
-		
-		System.out.println("master 작업 중");
-		System.out.println("moudule_B start");
-		
-		System.out.println("충돌 해결~~~ ㅎㅎㅎ");
-		
-		System.out.println("github 연동시작");
 	}
 }
