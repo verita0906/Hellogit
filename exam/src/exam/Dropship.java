f6376f94fb922599c100a6ea9fb5be317197909f
naverca <veritasqua@naver.com>
2021-08-27 19:18:11 +0900

    2번 문제 풀던 도중에 저장

---
+++ exam/src/exam/Dropship.java
@@ -0,0 +1,25 @@
+package exam;
+
+public class Dropship extends Unit{
+
+	int x, y;
+
+	@Override
+	void move(int x, int y) { 
+		System.out.println("주말");
+	}
+	
+	void stop() {
+
+	}
+	
+	void load() {
+	
+	}	
+	
+	void unload() {
+	
+	}
+
+
+}
---
+++ exam/src/exam/Tank.java
@@ -0,0 +1,21 @@
+package exam;
+
+public class Tank extends Unit {
+
+	int x, y;
+
+	
+	void stop() {
+
+	}
+	
+	void changMode() {
+	
+		
+	}
+
+	@Override
+	void move(int x, int y) {
+		System.out.println("하하하");
+	}	
+}
